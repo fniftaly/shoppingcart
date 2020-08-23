c19a275668d4da11c84ac5c787d052f1cc3dd602
Farad Niftaliyev <fngo@MacBook-Pro.local>
2020-08-22 17:45:19 -0700

    add animation and modal

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,30 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import formatCurrency from "./util";
-
+import Fade from "react-reveal/Fade";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
 const Products = ({ data, addToCart }) => {
+  const [product, setProduct] = useState(null);
+
+  const openModal = (product) => {
+    setProduct(product);
+  };
+  const closeModal = () => {
+    setProduct(null);
+  };
+  const handleModalClose = (product) => {
+    addToCart(product);
+    closeModal();
+  };
   return (
     <div>
-      <ul className="products">
-        {data.map((product) => (
-          <li key={product._id}>
-            <div className="product">
-              <a href={"#" + product._id}>
-                <img src={product.image} alt={product.title} />
-                <p>{product.title}</p>
-              </a>
-              <div className="product-price">
-                <div>{formatCurrency(product.price)}</div>
-                <button
-                  className="button primary"
-                  onClick={() => addToCart(product)}
-                >
-                  Add to cart
-                </button>
+      <Fade bottom cascade>
+        <ul className="products">
+          {data.map((product) => (
+            <li key={product._id}>
+              <div className="product">
+                <a href={"#" + product._id} onClick={() => openModal(product)}>
+                  <img src={product.image} alt={product.title} />
+                  <p>{product.title}</p>
+                </a>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button
+                    className="button primary"
+                    onClick={() => addToCart(product)}
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Fade>
+      {product && (
+        <Modal
+          isOpen={true}
+          onRequestClose={closeModal}
+          contentLabel="Modal"
+          appElement={document.getElementById("root")}
+        >
+          <Zoom>
+            <div>Modal View</div>
+            <button className="close-modal" onClick={closeModal}>
+              x
+            </button>
+            <div className="product-details">
+              <img src={product.image} alt={product.title} />
+              <div className="product-details-description">
+                <p>
+                  <strong>{product.title}</strong>
+                </p>
+                <p>{product.description}</p>
+                <p>
+                  Available Sizes
+                  {product.availableSizes.map((k) => (
+                    <span key={k}>
+                      {" "}
+                      <button className="button">{k}</button>
+                    </span>
+                  ))}
+                </p>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button
+                    className="button primary"
+                    onClick={() => handleModalClose(product)}
+                  >
+                    Add to cart
+                  </button>
+                </div>
               </div>
             </div>
-          </li>
-        ))}
-      </ul>
+          </Zoom>
+        </Modal>
+      )}
     </div>
   );
 };
